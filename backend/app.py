45719945631f53fe3cfbf737e99d1b0dbec18a12
Mikhail R <RyazanGrove@users.noreply.github.com>
2019-11-13 21:05:45 +0200

    first templates of endpoints

--- backend/app.py
+++ backend/app.py
@@ -22,7 +22,7 @@ ref = db.reference('/')
 @app.route('/set_data', methods=['GET'])
 def set_data():
     ref.set({
-        'boxes': 
+        'boxes':
             {
                 'box001': {
                     'color': 'red',
@@ -44,6 +44,7 @@ def set_data():
                 }
             }
         })
+    return "cya bloat'"
 
 @app.route('/update_data', methods=['GET'])
 def update_data():
@@ -53,6 +54,75 @@ def update_data():
         'color': 'test aaAA$$$$'
     })
     # pizda
+
+@app.route('/user_authentication')
+def user_authentication():
+    return "This is user_authentication method. returns fails or not"
+
+@app.route('/get_profile_settings')
+def get_profile_settings():
+    return "This is get_profile_settings method. returns profile info"
+
+@app.route('/set_profile_settings')
+def set_profile_settings():
+    return "This is set_profile_settings method. returns fails or not"
+
+@app.route('/create_project')
+def create_project():
+    return "This is create_project method. returns fails or not"
+
+@app.route('/set_member_to_project')
+def set_member_to_project():
+    return "This is set_member_to_project method. returns fails or not"
+
+@app.route('/get_member_to_project')
+def get_member_to_project():
+    return "This is get_member_to_project method. returns a list of members"
+
+@app.route('/set_task_to_project')
+def set_task_to_project():
+    return "This is set_task_to_project method. returns fails or not"
+
+@app.route('/get_task_to_project')
+def get_task_to_project():
+    return "This is get_task_to_project method. returns list of projects"
+
+@app.route('/convert_image_to_task')
+def convert_image_to_task():
+    return "This is convert_image_to_task method. returns fails or not"
+
+@app.route('/add_attachments_to_project')
+def add_attachments_to_project():
+    return "This is add_attachments_to_project method. returns fails or not"
+
+@app.route('/delete_project')
+def delete_project():
+    return "This is delete_project method. returns fails or not"
+
+@app.route('/show_project_content')
+def show_project_content():
+    return "This is show_project_content method. returns project contennt"
+
+@app.route('/generate_project_report')
+def generate_project_report():
+    return "This is generate_project_report method. returns fails or not or may be returns a report"
+
+@app.route('/get_list_of_projects')
+def get_list_of_projects():
+    return "This is get_list_of_projects method. returns list of created projects"
+
+@app.route('/search_for_project')
+def search_for_project():
+    return "This is search_for_project method. returns null or a project"
+
+@app.route('/get_image_resolution')
+def get_image_resolution():
+    return "This is get_image_resolution method. returns a new image"
+
+@app.route('/send_notification')
+def send_notification():
+    return "This is send_notification method. sends a notification"
+
 
 
 if __name__ == "__main__":
