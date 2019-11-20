d020f13188833f221e72cb1b03abc6c37d77e482
Subtselnyi Oleksandr <oleksandr.subtselnyi@aalto.fi>
2019-11-21 01:22:27 +0200

    first draft only image recognition

---
+++ frontend/app/src/main/java/com/mcc_project_5/Convert_image.kt
@@ -0,0 +1,82 @@
+package com.mcc_project_5
+
+import android.app.Activity
+import android.content.Intent
+import android.graphics.drawable.BitmapDrawable
+import android.os.Bundle
+import android.provider.MediaStore
+import android.view.View
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.Toast
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.ml.vision.FirebaseVision
+import com.google.firebase.ml.vision.common.FirebaseVisionImage
+import com.google.firebase.ml.vision.text.FirebaseVisionText
+
+import kotlinx.android.synthetic.main.activity_convert_image.*
+
+class Convert_image : AppCompatActivity() {
+
+    lateinit var imageView: ImageView
+    lateinit var editText: EditText
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        imageView = findViewById(R.id.imageView)
+        editText = findViewById(R.id.showText)
+    }
+
+
+    fun selectImage(v: View) {
+        val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+        intent.type = "image/*"
+        startActivityForResult(Intent.createChooser(intent, "Select Picture"), 1)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == 1 && resultCode == Activity.RESULT_OK) {
+            imageView.setImageURI(data!!.data)
+
+        }
+    }
+
+    fun startRecognizing(v: View) {
+        if (imageView.drawable != null) {
+            editText.setText("")
+            v.isEnabled = false
+            val bitmap = (imageView.drawable as BitmapDrawable).bitmap
+            val image = FirebaseVisionImage.fromBitmap(bitmap)
+            val detector = FirebaseVision.getInstance().onDeviceTextRecognizer
+
+            detector.processImage(image)
+                .addOnSuccessListener { firebaseVisionText ->
+                    v.isEnabled = true
+                    processResultText(firebaseVisionText)
+                }
+                .addOnFailureListener {
+                    v.isEnabled = true
+                    editText.setText("Failed")
+                }
+        } else {
+            Toast.makeText(this, "Select an Image First", Toast.LENGTH_LONG).show()
+        }
+
+    }
+
+
+    private fun processResultText(resultText: FirebaseVisionText) {
+        if (resultText.textBlocks.size == 0) {
+            editText.setText("No Text Found")
+            return
+        }
+        for (block in resultText.textBlocks) {
+            val blockText = block.text
+            editText.append(blockText)
+        }
+    }
+
+}
--- frontend/app/src/main/java/com/mcc_project_5/MainActivity.kt
+++ frontend/app/src/main/java/com/mcc_project_5/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.mcc_project_5
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.google.firebase.database.FirebaseDatabase
 
 
@@ -9,5 +11,13 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
+
+    fun change(v: View) {
+        val intent = Intent(this, Convert_image::class.java)
+        startActivity(intent);
+    }
+
+
 }
