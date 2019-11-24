a27a37e510d6b48a9f47b3479d9b00e1cab7d8c2
strazdv1 <strazdinavv@gmail.com>
2019-11-24 15:22:13 +0300

    Login and sing up were added

--- frontend/app/src/main/java/com/mcc_project_5/MainActivity.kt
+++ frontend/app/src/main/java/com/mcc_project_5/MainActivity.kt
@@ -2,12 +2,86 @@ package com.mcc_project_5
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.google.firebase.database.FirebaseDatabase
-
+import android.content.Intent
+import android.util.Patterns
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        auth = FirebaseAuth.getInstance()
+
+        btn_sign_up.setOnClickListener {
+            val intent = Intent(this, SignUpActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+        btn_log_in.setOnClickListener {
+            doLogin()
+        }
+
     }
+
+
+    private fun doLogin() {
+        if (tv_username.text.toString().isEmpty()) {
+            tv_username.error = "Please enter email"
+            tv_username.requestFocus()
+            return
+        }
+
+        if (!Patterns.EMAIL_ADDRESS.matcher(tv_username.text.toString()).matches()) {
+            tv_username.error = "Please enter valid email"
+            tv_username.requestFocus()
+            return
+        }
+
+        if (tv_password.text.toString().isEmpty()) {
+            tv_password.error = "Please enter password"
+            tv_password.requestFocus()
+            return
+        }
+
+        auth.signInWithEmailAndPassword(tv_username.text.toString(), tv_password.text.toString())
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    val user = auth.currentUser
+                    updateUI(user)
+                } else {
+
+                    updateUI(null)
+                }
+            }
+    }
+
+    public override fun onStart() {
+        super.onStart()
+        val currentUser = auth.currentUser
+        updateUI(currentUser)
+    }
+
+    private fun updateUI(currentUser: FirebaseUser?) {
+
+        if (currentUser != null) {
+            Toast.makeText(
+                baseContext, "Please verify your email address.",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+        else {
+            Toast.makeText(
+                baseContext, "Login failed.",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+    }
+
 }
