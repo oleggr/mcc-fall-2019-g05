3f4a1fbbec920fb94ee166e2669182827f142b78
strazdv1 <strazdinavv@gmail.com>
2019-12-07 20:07:10 +0300

    image view and buttons were added

--- frontend/app/src/main/java/com/mcc_project_5/ProfileSettingsActivity.kt
+++ frontend/app/src/main/java/com/mcc_project_5/ProfileSettingsActivity.kt
@@ -1,7 +1,12 @@
 package com.mcc_project_5
 
+import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
+import android.provider.MediaStore
+import android.view.View
+import android.widget.Button
+import android.widget.ImageView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.EmailAuthProvider
@@ -12,13 +17,35 @@ class ProfileSettingsActivity: AppCompatActivity(){
 
     private lateinit var auth: FirebaseAuth
 
+    lateinit var imageView: ImageView
+    private var upload: Button? = null
+    private val GALLERY = 1
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile_settings)
 
+        imageView = findViewById(R.id.imageView)
+        upload = findViewById<View>(R.id.upload) as Button
+        upload!!.setOnClickListener { choosePhotoFromGallary()}
+
         auth = FirebaseAuth.getInstance()
         btn_change_password.setOnClickListener {
             changePassword()
+        }
+    }
+
+    private fun choosePhotoFromGallary() {
+        val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+        intent.type = "image/*"
+        startActivityForResult(intent, GALLERY)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == 1 && resultCode == Activity.RESULT_OK) {
+            imageView.setImageURI(data!!.data)
+
         }
     }
 
