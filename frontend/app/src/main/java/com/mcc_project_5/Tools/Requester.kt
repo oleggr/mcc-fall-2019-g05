3fea36d99da8a21c1d9ff828598e2045923e410e
subtselnyi <oleksandr.subtselnyi@aalto.fi>
2019-12-07 23:30:21 +0200

    fixed token firebase

--- frontend/app/src/main/java/com/mcc_project_5/Tools/Requester.kt
+++ frontend/app/src/main/java/com/mcc_project_5/Tools/Requester.kt
@@ -10,6 +10,13 @@ import com.google.firebase.iid.InstanceIdResult
 import okhttp3.*
 import okhttp3.RequestBody
 import org.json.JSONObject
+import com.google.firebase.auth.GetTokenResult
+import androidx.annotation.NonNull
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.FirebaseUser
+
+
 
 
 class Requester(context: Context) {
@@ -19,78 +26,99 @@ class Requester(context: Context) {
     private val X_FIREBASE_TOKEN = "Firebase-token"
 
     fun httpGet(url: String, callBack: Callback) {
-        FirebaseInstanceId.getInstance().instanceId
-            .addOnSuccessListener(object : OnSuccessListener<InstanceIdResult> {
-                override fun onSuccess(instanceIdResult: InstanceIdResult) {
-                    val token = instanceIdResult.token //Token
-                    Log.d("token", token)
-                    val request = Request.Builder()
-                        .header(X_FIREBASE_TOKEN, token)
-                        .url(baseUrl + url)
-                        .get()
-                        .build()
+        val mUser = FirebaseAuth.getInstance().currentUser
+        mUser!!.getIdToken(true)
+            .addOnCompleteListener(object : OnCompleteListener<GetTokenResult> {
+                override fun onComplete(task: Task<GetTokenResult>) {
+                    if (task.isSuccessful()) {
+                        val idToken = task.getResult()?.getToken()
+                        Log.d("token", idToken)
+                        val request = Request.Builder()
+                            .header(X_FIREBASE_TOKEN, idToken!!)
+                            .url(baseUrl + url)
+                            .get()
+                            .build()
 
-                    val call = client.newCall(request)
-                    call.enqueue(callBack)
+                        val call = client.newCall(request)
+                        call.enqueue(callBack)
+                    } else {
+                        Log.d("token", "NO")
+                    }
                 }
             })
     }
 
     fun httpDelete(url: String, callBack: Callback) {
-        FirebaseInstanceId.getInstance().instanceId
-            .addOnSuccessListener(object : OnSuccessListener<InstanceIdResult> {
-                override fun onSuccess(instanceIdResult: InstanceIdResult) {
-                    val token = instanceIdResult.token //Token
+        val mUser = FirebaseAuth.getInstance().currentUser
+        mUser!!.getIdToken(true)
+            .addOnCompleteListener(object : OnCompleteListener<GetTokenResult> {
+                override fun onComplete(task: Task<GetTokenResult>) {
+                    if (task.isSuccessful()) {
+                        val idToken = task.getResult()?.getToken()
+                        Log.d("token", idToken)
+                        val request = Request.Builder()
+                            .header("User-Agent", "AndroidApp")
+                            .header(X_FIREBASE_TOKEN, idToken!!)
+                            .url(baseUrl + url)
+                            .delete()
+                            .build()
 
-                    val request = Request.Builder()
-                        .header("User-Agent", "AndroidApp")
-                        .header(X_FIREBASE_TOKEN, token)
-                        .url(baseUrl + url)
-                        .delete()
-                        .build()
-
-                    val call = client.newCall(request)
-                    call.enqueue(callBack)
+                        val call = client.newCall(request)
+                        call.enqueue(callBack)
+                    } else {
+                        Log.d("token", "NO")
+                    }
                 }
             })
 
     }
 
     fun httpPost(url: String, json: JSONObject, callBack: Callback) {
-        FirebaseInstanceId.getInstance().instanceId
-            .addOnSuccessListener(object : OnSuccessListener<InstanceIdResult> {
-                override fun onSuccess(instanceIdResult: InstanceIdResult) {
-                    val token = instanceIdResult.token //Token
-                    val body = RequestBody.create(JSON, json.toString())
+        val mUser = FirebaseAuth.getInstance().currentUser
+        mUser!!.getIdToken(true)
+            .addOnCompleteListener(object : OnCompleteListener<GetTokenResult> {
+                override fun onComplete(task: Task<GetTokenResult>) {
+                    if (task.isSuccessful()) {
+                        val idToken = task.getResult()?.getToken()
+                        Log.d("token", idToken)
+                        val body = RequestBody.create(JSON, json.toString())
 
-                    val request = Request.Builder()
-                        .header(X_FIREBASE_TOKEN, token)
-                        .url(baseUrl + url)
-                        .post(body)
-                        .build()
+                        val request = Request.Builder()
+                            .header(X_FIREBASE_TOKEN, idToken!!)
+                            .url(baseUrl + url)
+                            .post(body)
+                            .build()
 
-                    val call = client.newCall(request)
-                    call.enqueue(callBack)
+                        val call = client.newCall(request)
+                        call.enqueue(callBack)
+                    } else {
+                        Log.d("token", "NO")
+                    }
                 }
             })
 
     }
 
     fun httpDownload(url: String, callBack: Callback) {
-        FirebaseInstanceId.getInstance().instanceId
-            .addOnSuccessListener(object : OnSuccessListener<InstanceIdResult> {
-                override fun onSuccess(instanceIdResult: InstanceIdResult) {
-                    val token = instanceIdResult.token //Token
-                    val request = Request.Builder()
-                        .header("User-Agent", "AndroidApp")
-                        .header(X_FIREBASE_TOKEN, token)
-                        .url(baseUrl + url)
-                        .get()
-                        .build()
+        val mUser = FirebaseAuth.getInstance().currentUser
+        mUser!!.getIdToken(true)
+            .addOnCompleteListener(object : OnCompleteListener<GetTokenResult> {
+                override fun onComplete(task: Task<GetTokenResult>) {
+                    if (task.isSuccessful()) {
+                        val idToken = task.getResult()?.getToken()
+                        Log.d("token", idToken)
+                        val request = Request.Builder()
+                            .header("User-Agent", "AndroidApp")
+                            .header(X_FIREBASE_TOKEN, idToken!!)
+                            .url(baseUrl + url)
+                            .get()
+                            .build()
 
-                    val call = client.newCall(request)
-                    call.enqueue(callBack)
-
+                        val call = client.newCall(request)
+                        call.enqueue(callBack)
+                    } else {
+                        Log.d("token", "NO")
+                    }
                 }
             })
 
